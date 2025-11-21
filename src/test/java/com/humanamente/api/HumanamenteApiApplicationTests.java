627dc61688cac30032982b489983b77753dd38b0
MUKINH4 <samuelmaragato3@gmail.com>
2025-11-21 17:50:57 -0300

    fix

--- src/test/java/com/humanamente/api/HumanamenteApiApplicationTests.java
+++ src/test/java/com/humanamente/api/HumanamenteApiApplicationTests.java
@@ -1,13 +1,23 @@
 package com.humanamente.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+	webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+	properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration"
+	}
+)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc(addFilters = false)
 class HumanamenteApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Test that Spring context loads successfully
 	}
 
 }
